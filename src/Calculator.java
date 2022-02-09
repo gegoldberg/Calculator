f26e3923bf3195d48d758015b6da6dabad35ee81
Garrett <ggoldbergtwin@gmail.com>
2022-02-08 23:24:20 -0500

    Added prompt to add or subtract based off of user input.

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,11 +4,22 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Enter in a number");
 		int operand1 = sc.nextInt();
 		System.out.println("Enter in a number");
 		int operand2 = sc.nextInt();
-		System.out.println((operand1 + operand2));
+		
+		Scanner sr = new Scanner(System.in);
+		System.out.println("Would you like to add or subtract?");
+		String choice = sr.nextLine();
+		if (choice.equalsIgnoreCase("add")) {
+			System.out.println((operand1 + operand2));
+		}
+		else if (choice.equalsIgnoreCase("subtract")) {
+			System.out.println((operand1 + operand2));
+		}
+	
 	}
 
 }
